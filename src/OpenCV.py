e2fce4af4eaac5238b0cf81d1b1a183d4a7c8c33
Alexander Webber <alexander.webber5225@gmail.com>
2019-09-21 12:45:33 -0700

    video capture

--- src/OpenCV.py
+++ src/OpenCV.py
@@ -1,1 +1,17 @@
-print("Hello world")
+import cv2
+
+#creates variable that reads lena.jpg
+img = cv2.imread('lena.jpg', 0)
+
+print(img)
+
+#shows lena.jpg in a window called image
+cv2.imshow('image', img)
+k = cv2.waitKey(0)
+
+#makes copy of lena.jpg when the "s" key is pressed
+if k == 27:
+    cv2.destroyAllWindows()
+elif k == ord('s'):
+    cv2.imwrite('lena_copy.jpg', img)
+cv2.destroyAllWindows()
